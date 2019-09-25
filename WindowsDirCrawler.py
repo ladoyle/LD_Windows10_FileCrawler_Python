49c7805afb162e708994120ca14668a716797d28
haelfeyn <healfein@gmail.com>
2019-09-25 15:53:39 -0700

    Added an image file for testing. Updated the crawler to recursively search for files and place them into the 'words' folder, which is created on start.

--- WindowsDirCrawler.py
+++ WindowsDirCrawler.py
@@ -1,11 +1,55 @@
+#!/usr/bin/env python3
 import os
 
-print('\n' + os.getcwd())
 
-os.chdir('C:/Users/healf/git/LD_Windows10_FileCrawler_Python/tests')
-print(os.stat('text.txt'))
+pics_vids = ''
+pdfs = ''
+words = ''
+sheets = ''
 
-for dirpath, filenames in os.walk('C:/Users/healf/git/'):
-    print('Current Path:= ', dirpath)
-    print('File:= ', filenames)
-    print()
+
+def organize_desktop(path):
+    global pics_vids
+    global words
+    global pdfs
+    global sheets
+    os.chdir(path)
+
+    for entry in os.scandir(path=path):
+        if os.path.isdir(entry):
+            if 'pics_vids' is str(entry.name):
+                pass
+            elif 'words' is str(entry.name):
+                pass
+            elif 'pdfs' is str(entry.name):
+                pass
+            elif 'sheets' is str(entry.name):
+                pass
+            else:
+                organize_desktop(os.path.join(path, entry))
+        else:
+            src = str(os.path.join(path, entry))
+            dest = str(words) + str(entry.name)
+            os.rename(src, dest)
+
+
+if __name__ == '__main__':
+    start_path = 'C:/Users/healf/git/LD_Windows10_FileCrawler_Python/tests/'
+
+    try:
+        pics_vids = os.path.join(start_path, 'pics_vids/')
+        words = os.path.join(start_path, 'words/')
+        pdfs = os.path.join(start_path, 'pdfs/')
+        sheets = os.path.join(start_path, 'sheets/')
+
+        os.mkdir(pics_vids)
+        os.mkdir(words)
+        os.mkdir(pdfs)
+        os.mkdir(sheets)
+    except FileExistsError as exception:
+        print(exception)
+        print('One of the directories already exists!!')
+
+    organize_desktop(start_path)
+else:
+    print('This module is not meant to be imported yet...')
